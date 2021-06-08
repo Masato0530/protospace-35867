49853c5787dae2ffd4f7b7a6f477604968d8a58e
Masato0530 <o.masato3536@icloud.com>
2021-06-08 11:37:57 +0900

    ログイン機能修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
-class CommentsController < ApplicationController
+class CommentsController < ApplicationController 
   def create
     @comment = Comment.new(comment_params)
     if @comment.save  
     redirect_to prototype_path(@comment.prototype)
     else
-      @prototype = @comment.tweet
+      @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show"
     end
